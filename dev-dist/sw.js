d7a3a53ddefb2cb94925cef70ba70bdddb0cedb4
Eben Mallory <ebenryanmallory@proton.me>
2025-07-16 09:55:41 -0500

    07/16 update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qvgi7gp1log"
+    "revision": "0.npdevhruc4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
